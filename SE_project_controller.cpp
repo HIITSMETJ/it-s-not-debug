dea241a5d346565a0afd323b11bb0c00c7d2c6f7
B10432006 <sunnyfordata1209@gmail.com>
2018-01-06 11:04:20 +0800

    origin

---
+++ SE_project_controller.cpp
@@ -0,0 +1,25 @@
+#include <windows.h>
+#include <conio.h>
+#include <ctime>
+#include <cmath>
+#include <iostream>
+#include "SE_Project_model.cpp"
+
+using namespace std;
+
+class TetrisController
+{
+public:
+	TetrisController(TetrisModel* model, int board_width, int board_height);
+
+
+
+private:
+	TetrisModel * _model;//??
+
+};
+
+TetrisController::TetrisController(TetrisModel* model, int board_width, int board_height)
+{
+	//current_block
+}
