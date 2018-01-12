0b38dd899c08a2aa08cc559f4242f91ee5b35c1c
JamesLi0623 <35157744+JamesLi0623@users.noreply.github.com>
2018-01-12 12:55:34 +0800

    view for change

---
+++ Tetris/Tetris/B10432028.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Tetris
+{
+    partial class B10432028
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.Score = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.MediumPurple;
+            this.Start.FlatAppearance.BorderSize = 0;
+            this.Start.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Start.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Start.Location = new System.Drawing.Point(330, 71);
+            this.Start.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(123, 54);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.strat_Click);
+            this.Start.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyPress);
+            // 
+            // Score
+            // 
+            this.Score.AutoSize = true;
+            this.Score.CausesValidation = false;
+            this.Score.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Score.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Score.Location = new System.Drawing.Point(330, 13);
+            this.Score.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Score.Name = "Score";
+            this.Score.Size = new System.Drawing.Size(133, 51);
+            this.Score.TabIndex = 1;
+            this.Score.Text = "Score:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(312, 456);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Designed by";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(325, 496);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 30);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "THIS IS NOT DEBUG";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // B10432028
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(572, 635);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Score);
+            this.Controls.Add(this.Start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "B10432028";
+            this.Text = "Tetris :: THIS IS NOT DEBUG";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        protected internal System.Windows.Forms.Label Score;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tetris/Tetris/B10432028.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tetris
+{
+    //public class TetrisView : Form
+    //{
+    //    public virtual void changeView(TetrisModel model) { }
+    //}
+
+
+
+
+    public partial class B10432028 : TetrisView //TetrisView
+    {
+        Graphics graphics;
+        TetrisController controller;
+        // Create pen.
+        Pen blackPen = new Pen(Color.White, 3);  //畫筆顏色
+        Pen redPen = new Pen(Color.Black, 3);    //落下方塊顏色
+        SolidBrush brush = new SolidBrush(Color.Gold);  //??
+
+        //for double buffer
+        private BufferedGraphicsContext m_CurrentContext;
+        private BufferedGraphics m_Graphics;
+
+        public B10432028(TetrisController controller)
+        {
+            InitializeComponent();
+
+            //get畫布
+            graphics = this.CreateGraphics();
+
+            //塗白底
+            graphics.Clear(Color.White);
+
+            //double buffer
+            m_CurrentContext = BufferedGraphicsManager.Current;
+            m_Graphics = m_CurrentContext.Allocate(this.CreateGraphics(), this.DisplayRectangle);
+
+            drawBackGround();
+            this.controller = controller;
+        } 
+
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            graphics.Clear(Color.Black);  //起始背景色
+            drawBackGround();
+            //drawBlock(170, 20);
+
+        }
+
+
+        private void strat_Click(object sender, EventArgs e)
+        {
+            controller.start();
+        }
+
+        public void drawBackGround()                //畫框線
+        {
+            // Create points that define line.
+            m_Graphics.Graphics.DrawLine(blackPen, 30, 20, 200, 20);  //上
+            m_Graphics.Graphics.DrawLine(blackPen, 200, 20, 200, 394);  //右
+            m_Graphics.Graphics.DrawLine(blackPen, 200, 394, 30, 394);  //下
+            m_Graphics.Graphics.DrawLine(blackPen, 30, 394, 30, 20);  //左
+        }
+
+        private void Form1_KeyPress(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            controller.keyPress(e.KeyCode.ToString());
+        }
+
+        public void drawBlock(Pen pen, int i, int j)   //??
+        {
+            i = i * 17 + 20;
+            j = j * 17 + 30;
+            m_Graphics.Graphics.FillRectangle(brush, j, i, 17, 17);   //畫方塊
+            m_Graphics.Graphics.DrawRectangle(pen, j, i, 17, 17);   //畫方塊
+        }
+
+        //public override void changeView(TetrisModel model) 多型覆蓋
+        public override void changeView(TetrisModel model)
+        {
+            m_Graphics.Graphics.Clear(Color.Black);   //背景這也要改
+            drawBackGround();
+            bool[,] board = model.getBoard();
+            TetrisBlock current_block = model.getBlock();
+
+            for (int i = 0; i < 22; i++) 
+            {
+                for (int j = 0; j < 10; j++) 
+                {
+                    if (board[i, j])
+                    {
+                        drawBlock(blackPen, i, j);
+                    }
+                }
+            }
+
+            for (int i = 0; i < 4; i++) 
+            {
+                drawBlock(redPen, current_block.get_CurrentX() + current_block.get_X(i), current_block.get_CurrentY() + current_block.get_Y(i));
+
+            }
+            m_Graphics.Render();
+            //m_Graphics.Dispose();
+            Score.Text = "Score: " + model.getScore().ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //private void InitializeComponent()
+        //{
+        //    this.SuspendLayout();
+        //    // 
+        //    // B10432028
+        //    // 
+        //    this.ClientSize = new System.Drawing.Size(282, 253);
+        //    this.Name = "B10432028";
+        //    //this.Load += new System.EventHandler(this.B10432028_Load);
+        //    this.ResumeLayout(false);
+
+        //}
+
+
+    }
+
+}
